d14617421ed42e739e75c81029572d4bacdd4529
barsu5489 <ebarsulai@gmail.com>
2023-02-28 22:05:33 +0300

    skills section added with limited styling

--- frontend/src/components/Skills.js
+++ frontend/src/components/Skills.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <div>Skills
+        <div class="skills">
+  <h2 class="skills__title">Skills</h2>
+  <ul class="skills__list">
+    <li class="skills__item">HTML</li>
+    <li class="skills__item">CSS</li>
+    <li class="skills__item">JavaScript</li>
+    <li class="skills__item">JavaScript</li>
+    <li class="skills__item">JavaScript</li>
+     <li class="skills__item">JavaScript</li>
+  </ul>
+  <button class="skills__button" onclick="addSkill()">Add Skill</button>
+</div>
+    </div>
   )
 }
 
