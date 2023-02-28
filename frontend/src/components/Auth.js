073a1f3aebf71c8726669f26fe1c91111da0da79
barsu5489 <ebarsulai@gmail.com>
2023-02-28 23:20:01 +0300

    code cleanup and nameing

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -2,11 +2,25 @@ import React from 'react'
 
 function Auth() {
   return (
-    <div>
-        <div className="login-form"></div>
-        <div className="signup-form"></div>
+    <>
+    <div className='authenticate'>
+        <div className="login-form">
+        <input type="email" name="" id="" placeholder='email'/>
+        <input type="name" name="" id="" placeholder='username'/>
+        <input type="password" placeholder='password'/>
+        <button type="submit">Login</button>
+        </div>
+        <div className="signup-form">
+        <input type="email" name="" id="" placeholder='email'/>
+        <input type="name" name="" id="" placeholder='username'/>
+        <input type="password" placeholder='password'/>
+        <input type="password" placeholder='confirm password'/>
+        <button type="submit">signup</button>
+
+        </div>
 
     </div>
+    </>
   )
 }
 
