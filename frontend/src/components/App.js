d52d12a8df0f12f4e59a73ab59e55e5136c8d8e2
barsu5489 <ebarsulai@gmail.com>
2023-02-28 20:00:21 +0300

    navbar setup and css styling

---
+++ frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+     <h1>Header</h1>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+    <nav>
+  <div class="navbar-logo">
+    <a href="#">Emmanuel Barsu</a>
+  </div>
+  <div class="navbar-links">
+    <a href="#">Currently in Kenya</a>
+    <a href="#">Currently working<br /> on REACT</a>
+    <a href="#">My Projects</a>
+    <a href="#">Skills</a>
+  </div>
+</nav>
+</>
+  )
+}
+
+export default Header
