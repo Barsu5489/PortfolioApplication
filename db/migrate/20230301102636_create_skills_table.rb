005c4a29313f66066174e91a4bfe166c8b92dc51
barsu5489 <ebarsulai@gmail.com>
2023-03-01 14:55:47 +0300

    skills table created

--- db/migrate/20230301102636_create_skills_table.rb
+++ db/migrate/20230301102636_create_skills_table.rb
@@ -1,4 +1,9 @@
 class CreateSkillsTable < ActiveRecord::Migration[6.1]
   def change
+    create_table :skills do |t|
+      t.string :name
+      t.integer :user_id
+      t.timestamps
+    end
   end
 end
